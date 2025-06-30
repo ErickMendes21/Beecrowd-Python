9a73da5332992c1d28faad8f0ee8bc9fc2aee7b7
ErickMendes21 <erickmendesrocha15@gmail.com>
2025-06-30 20:14:40 -0300

    Substituição-em-Vetor-1172

--- Substituição-em-Vetor-1172.py
+++ Substituição-em-Vetor-1172.py
@@ -1,14 +1,3 @@
-# X = list(map(int, input().split()))
-# for i in range(len(X)):
-#     if X[i] <= 0:
-#         X[i] = 1
-#     print(f"X[{i}] = {X[i]}")
-
-# for j, i in enumerate(X):
-#     if i <= 0:
-#         i = 1
-#     print(f"X[{j}] = {i}")
-
 X = []
 for j, i in enumerate(X):
     valor = int(input())
